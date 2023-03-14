85f5e9d935aae7ef4ffc30effe5d432575525a6d
yeyueluke <12543897+yeyueluke@users.noreply.github.com>
2023-03-14 20:31:42 +0800

    Update thread_safe_queue_test.cpp

--- thread_safe_queue_test.cpp
+++ thread_safe_queue_test.cpp
@@ -3,28 +3,37 @@
 #include <thread>
 
 int main() {
-	std::cout << "threadsafe_queue test!" << std::endl;
+    std::cout << "threadsafe_queue test!" << std::endl;
 	RGR::threadsafe_queue<size_t> myQueue;
-  
+
 	std::thread td01([&] {
 		for (size_t i = 0; i < 10; i++) {
 			myQueue.push(i);
 		}
 	});
-  
+
 	std::thread td02([&] {
 		for (size_t i = 10; i < 20; i++) {
 			myQueue.push(i);
 		}
 	});
-  
+
 	td01.join();
-	td02.join();// if use detach() wont get all output
+	td02.join();
 	std::cout << "myQueue, size: " << myQueue.size() << std::endl;
 	size_t myQueueLen = myQueue.size();
 	for (size_t i = 0; i < myQueueLen; i++) {
 		std::cout << myQueue.wait_and_pop() << " size: " << myQueue.size() << std::endl;
 	}
+	#if 0  //// if use detach() 
+	while(ture) {
+		if(!myQueue.empty()) {
+		    std::cout << myQueue.wait_and_pop() << " size: " << myQueue.size() << std::endl;
+		} else {
+			break;
+		}
+	}
+	#endif
 	std::getchar();
   return 0;
 }
